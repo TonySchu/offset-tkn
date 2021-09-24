a561b36c82448bf59878d622a0ce5c549b7d4bbb
JohnTeclead <yjohn.einicke@teclead.de>
2021-09-24 13:39:04 +0200

    readded changes to the unused elements

--- pages/components/DefaultTemplate.tsx
+++ pages/components/DefaultTemplate.tsx
@@ -5,26 +5,30 @@ import Footer from "./Footer/Footer";
 import Navbar from "./Navbar/Navbar";
 
 export const SEO: SearchEngineOptimization = {
-  title: "Shape",
-  keywords: "noindex, nofollow",
-  description: "All Rights Reserved ©️ Teclead GmbH",
-  icon: "../assets/img/favicons/favicon.ico",
+    title: "Shape",
+    keywords: "noindex, nofollow",
+    description: "All Rights Reserved ©️ Teclead GmbH",
+    icon: "../assets/img/favicons/favicon.ico",
 };
-function DefaultTemplate(props: any) {
-  return (
-    <main className="main" id="top">
-      <Head>
-        <title>{props.SEO.title}</title>
-        <meta name="robots" content="noindex, nofollow" />
-        <meta name="description" content={props.SEO.description} />
-        <meta name="keywords" content={props.SEO.keywords} />
-        <link rel="icon" href={props.SEO.icon} />
-      </Head>
-      <Navbar />
-      {props.children}
-      <Footer />
-    </main>
-  );
+
+type Props = {
+    SEO: SearchEngineOptimization;
+};
+function DefaultTemplate(props: React.PropsWithChildren<Props>) {
+    return (
+        <main className="main" id="top">
+            <Head>
+                <title>{props.SEO.title}</title>
+                <meta name="robots" content="noindex, nofollow" />
+                <meta name="description" content={props.SEO.description} />
+                <meta name="keywords" content={props.SEO.keywords} />
+                <link rel="icon" href={props.SEO.icon} />
+            </Head>
+            <Navbar />
+            {props.children}
+            <Footer />
+        </main>
+    );
 }
 
 export default DefaultTemplate;
